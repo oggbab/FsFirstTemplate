cd3399866d96bdc16b2b1186680bbf2b72fef621
DESKTOP-K89B7F1\USER <wildwaves76@gmail.com>
2022-04-27 11:23:25 +0900

    template package 추가

---
+++ app/src/main/java/com/fsoon/android/fsfirsttemplate/view/tamplate/FixedHeaderActivity.java
@@ -0,0 +1,14 @@
+package com.fsoon.android.fsfirsttemplate.view.tamplate;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FixedHeader extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fixed_header);
+    }
+}
